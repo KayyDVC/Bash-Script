fe259b0694cf1e48bbd6a6f6ce6add98820efccc
KayyDVC <KayyDV1016@gmail.com>
2021-11-16 23:20:16 -0500

    Final M-P-A

--- mac-printer-authentication.sh
+++ mac-printer-authentication.sh
@@ -1,24 +1,23 @@
-#---------------------------------------------------------------¬
-#	Authors:  	Malakai Stanisclaus			|
+#------------------------------------------------------------------------------------------------------------------------------------|
+#	Authors:  	KayyDVC		|
 #	Date created:	11/8/21				|
 #	Purpose:	To Fix The "Hold for authentication" error on Mac OS	|
-#   Source Code From: https://www.papercut.com/kb/Main/HoldforAuthentication#what-does-ldquo-hold-for-authentication-rdquo-mean
+#   Source Code From: https://www.papercut.com/kb/Main/HoldforAuthentication#what-does-ldquo-hold-for-authentication-rdquo-mean |
 #
-#---------------------------------------------------------------┘
+#------------------------------------------------------------------------------------------------------------------------------------|
 #!/bin/bash
 
 sudo -v #Gives 5 minutes until the user must enter their PW for sudo perms again; If this time is not enough, the terminal will prompt for pw again and reset the timer.
 
-printers=`lpstat -s| grep -w -o "[%_[:alnum:]]*HP_[%_[:alnum:]]*"` #Change 'HP_' portion to general starting pattern of Canon Printers.
-#Creates an array of printer names (ie Canon%20Psychology)
+printers=`lpstat -s| grep -w -o "[%_[:alnum:]]*Canon_[%_[:alnum:]]*"` #Creates an array of printer names (ie Canon%20Psychology)
 
-#[%_[:alnum:]]* should allow for % signs, integers, and letters (UNTESTED)
+#[%_[:alnum:]]* should allow for % signs, integers, and letters 
 
-echo $printers #Used for testing, to be commented out.
+#echo $printers #Used for testing, to be commented out.
 
 for printer in $printers
 do
-    echo "sudo lpadmin -p ${printer[@]} -o auth-info-required=username,password" #remove echo and quotes after testing 
+    sudo lpadmin -p ${printer[@]} -o auth-info-required=username,password #sets the printer_policy to require user/pw on next print
 
 done
 
